5edb192fdfa32f5cae22a79f17035065adf8f891
Yacobus Reinhart <yacobus.reinhart@gmail.com>
2012-07-26 19:32:05 +0700

    extend new data

--- lib/finforenet/models/co_code.rb
+++ lib/finforenet/models/co_code.rb
@@ -3,9 +3,9 @@ module Finforenet
   module Models
     module CoCode
       def self.company_twitter(path="")
-      	infos = FeedInfo.where({:category => /(company|index|currency)/i})
-      	infos.map{|info| info.destroy}
-      	path = "#{Rails.root}/company.csv" if path.blank?
+        infos = FeedInfo.where({:category => /(company|index|currency)/i})
+        infos.map{|info| info.destroy}
+        path = "#{Rails.root}/company.csv" if path.blank?
         csv_file = File.new(path).read
         csv_data = CSV.parse(csv_file)
         header = Hash[csv_data.shift.each_with_index.map{| val,key | [val.downcase.gsub(" ","_"), key]}].to_options
@@ -27,18 +27,50 @@ module Finforenet
         			company = CompanyCompetitor.create({:keyword                => row[header[:competitor_keyword]],
         				                                  :competitor_ticker      => row[header[:competitor_ticker]],
         				                                  :company_keyword        => row[header[:keyword]],
-																								  :broadcast_keyword      => row[header[:broadcast_keyword]],
-																								  :finance_keyword        => row[header[:finance_keyword]],
-																								  :bing_keyword           => row[header[:bing_keyword]],
-																								  :blog_keyword           => row[header[:blog_keyword]],
-																								  :company_ticker         => row[header[:ticker]],
-																								  :feed_info              => feed_info.id
-        				                                 })
-        		end
-        	end
-        end
-      end
-
+                      											      :broadcast_keyword      => row[header[:broadcast_keyword]],
+                      											      :finance_keyword        => row[header[:finance_keyword]],
+                      											      :bing_keyword           => row[header[:bing_keyword]],
+                      											      :blog_keyword           => row[header[:blog_keyword]],
+                      											      :company_ticker         => row[header[:ticker]],
+                      											      :feed_info              => feed_info.id
+                              				          })
+        	    end
+        	  end
+         end
+       end
+       
+       def self.update_company(path="")
+         header = {:title => 0, :twitter => 1, :ticker => 2, :competitor_ticker => 3, :competitor_keyword => 4}
+         path = "#{Rails.root}/fastnd_extended.csv" if path.blank?
+         csv_file = File.new(path).read
+         csv_data = CSV.parse(csv_file)
+         csv_data.shift if csv_data.first[0] =~ /title/i
+         csv_data.each do |row|
+           company = CompanyCompetitor.where(:finance_keyword => row[header[:ticker]]).first
+           info_obj = {:address  => row[header[:twitter]], 
+                       :title    => row[header[:title]], 
+                       :category => "Company"
+                      }
+           company_obj = {:keyword           => row[header[:competitor_keyword]].split(/\s/).join(","),
+                          :competitor_ticker => row[header[:competitor_ticker]].split(/\s/).join(","),
+                          :company_keyword   => "#{row[header[:ticker]]},\"#{row[header[:title]]}\"",
+                          :broadcast_keyword => row[header[:title]],
+                          :finance_keyword   => row[header[:ticker]],
+                          :bing_keyword      => "#{row[header[:ticker]]},#{row[header[:title]]}",
+                          :blog_keyword      => "#{row[header[:ticker]]},#{row[header[:title]]}",
+                          :company_ticker    => row[header[:ticker]]
+                          }
+           if company
+             info = company.feed_info
+             info.update_attributes(info_obj)
+             company.update_attributes(company_obj)
+           else
+             info = FeedInfo.create(info_obj)
+             CompanyCompetitor.create(company_obj.merge!(:feed_info_id => info.id)) if info.valid?
+           end
+         end
+       end
+       
     end
   end
 end
